1fea80c69d26d99699e04a1c294f517df4098882
Adriana Jimenez Mangas <ajmangas@gmail.com>
2015-05-18 15:22:15 -0700

    Create a table, add add and edit buttons, tap cells to make them green

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -8,20 +8,79 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate>
 
+@property (weak, nonatomic) IBOutlet UITextField *enterTaskTextField;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@property NSMutableArray *tasks;
+@property BOOL isEditing;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.tasks = [NSMutableArray arrayWithObjects:@"Task 1",  @"Task 2", @"Task 3", @"Task 4", nil];
+    self.isEditing = NO;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.tasks.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    NSString *task = [self.tasks objectAtIndex:indexPath.row];
+    cell.textLabel.text = task;
+    return cell;
+}
+
+- (IBAction)onAddButtonPressed:(UIButton *)sender {
+    [self.tasks addObject:self.enterTaskTextField.text];
+    self.enterTaskTextField.text = @"";
+    [self.view endEditing:YES];
+    [self.tableView reloadData];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    cell.textLabel.textColor = [UIColor greenColor];
+    [tableView deselectRowAtIndexPath:indexPath animated:NO];
+}
+
+
+- (IBAction)onEditButtonPressed:(UIButton *)sender {
+    if (!self.isEditing) {
+        [sender setTitle:@"Done" forState:UIControlStateNormal];
+    } else {
+        [sender setTitle:@"Edit" forState:UIControlStateNormal];
+    }
+    self.isEditing = !self.isEditing;
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
