69788584c9776ba583bf44d38a0a17cdfa04927f
Adriana Jimenez Mangas <ajmangas@gmail.com>
2015-05-18 14:13:33 -0700

    Initial Commit

---
+++ Deprocrastinator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Deprocrastinator
+//
+//  Created by Adriana Jimenez Mangas on 5/18/15.
+//  Copyright (c) 2015 Adriana Jimenez Mangas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
